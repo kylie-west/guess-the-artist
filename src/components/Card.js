5289343ebaf8336b9f07186790bdd68b137ae46a
Kylie West <kmichellewest@gmail.com>
2023-03-20 15:59:34 -0700

    Create basic Card component

---
+++ src/components/Card.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Card = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	width: ${({ width }) => width || "auto"};
+	height: ${({ height }) => height || "auto"};
+	margin: ${({ margin }) => margin || "0px"};
+	padding: ${({ padding }) => padding || "0px"};
+	background: ${({ bg }) => bg || "white"};
+	border: 1px solid gray;
+	border-radius: 4px;
+`;
+
+export default Card;
