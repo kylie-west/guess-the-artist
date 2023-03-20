80faca6ead9529ebb770f1894554fb868fa87b3d
Kylie West <kmichellewest@gmail.com>
2023-03-20 13:27:20 -0700

    Copy code from Home.js to App.js, commenting out for now

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,48 @@ const App = () => {
 		numArtists: 2
 	});
 
+	// const loadGenres = async t => {
+	//   setConfigLoading(true)
+	//   const response = await fetchFromSpotify({
+	//     token: t,
+	//     endpoint: 'recommendations/available-genre-seeds'
+	//   })
+	//   console.log(response)
+	//   setGenres(response.genres)
+	//   setConfigLoading(false)
+	// }
+
+	// useEffect(() => {
+	// 	setAuthLoading(true);
+
+	// 	const storedTokenString = localStorage.getItem(TOKEN_KEY);
+	// 	if (storedTokenString) {
+	// 		const storedToken = JSON.parse(storedTokenString);
+	// 		if (storedToken.expiration > Date.now()) {
+	// 			console.log("Token found in localstorage");
+	// 			setAuthLoading(false);
+	// 			setToken(storedToken.value);
+	// 			loadGenres(storedToken.value);
+	// 			return;
+	// 		}
+	// 	}
+	// 	console.log("Sending request to AWS endpoint");
+	// 	request(AUTH_ENDPOINT).then(({ access_token, expires_in }) => {
+	// 		const newToken = {
+	// 			value: access_token,
+	// 			expiration: Date.now() + (expires_in - 20) * 1000
+	// 		};
+	// 		localStorage.setItem(TOKEN_KEY, JSON.stringify(newToken));
+	// 		setAuthLoading(false);
+	// 		setToken(newToken.value);
+	// 		loadGenres(newToken.value);
+	// 	});
+	// }, []);
+
+	// if (authLoading || configLoading) {
+	//   return <div>Loading...</div>
+	// }
+
 	return (
 		<div>
 			<Route exact path="/" component={Home} />
