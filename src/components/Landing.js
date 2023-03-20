f76ba80acab27de96f86323d1f6547b1f00a650b
marill300 <66380564+marill300@users.noreply.github.com>
2023-03-20 18:08:21 -0400

    adjusted formatting on landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,19 +1,20 @@
 import { Route } from "react-router-dom";
 import React, { useEffect, useState } from 'react'
+import styled from "styled-components";
 
 
 export const Landing = () =>{
 
     
 return (
-    <div>  
+    <Wrapper>  
         <h1>Who's Who</h1>
         <h2>A musical guessing game</h2>
 
         <h3>Pick a genre</h3>
 
         <h3> Songs per guess</h3>
-        <div class = "songsetting">
+        <SongSetting>
             <div>
                 <p> 1</p>
             </div>
@@ -23,9 +24,9 @@ return (
             <div>
                 <p> 3</p>
             </div>
-        </div>
+        </SongSetting>
         <h3> Artists per guess</h3>
-        <div class = "artistsetting">
+        <ArtistSetting>
             <div>
                 <p> 1</p>
             </div>
@@ -35,13 +36,13 @@ return (
             <div>
                 <p> 3</p>
             </div>
-        </div>
+        </ArtistSetting>
 
 
         
         <button>Confirm</button>
 
-    </div>
+    </Wrapper>
 
 
 
@@ -49,14 +50,27 @@ return (
 
 
 
-
-
-
-
-
-
-
-
 }
 
 export default Landing
+
+const SongSetting = styled.div`
+	display: flex;
+	justify-content: space-between;
+    align-items: center;
+	width: 20%;
+`;
+
+const ArtistSetting = styled.div`
+	display: flex;
+	justify-content: space-between;
+    align-items: center;
+	width: 20%;
+`;
+
+const Wrapper = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+gap: 50px;
+`;
