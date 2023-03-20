081c90759ff6af4a53dd81d6d3087929bd96485f
Kylie West <kmichellewest@gmail.com>
2023-03-20 13:21:38 -0700

    Set up state in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
 
-import Home from './Home'
+import Home from "./Home";
 
-const App = () => (
-  <div>
-    <Route exact path='/' component={Home} />
-  </div>
-)
+const App = () => {
+	const [token, setToken] = useState(null);
+	const [genres, setGenres] = useState([]);
+	const [authLoading, setAuthLoading] = useState(false);
+	const [configLoading, setConfigLoading] = useState(false);
+	const [config, setConfig] = useState({
+		selectedGenre: null,
+		numSongs: 1,
+		numArtists: 2
+	});
 
-export default App
+	return (
+		<div>
+			<Route exact path="/" component={Home} />
+		</div>
+	);
+};
+
+export default App;
