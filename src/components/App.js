ee783216904c53eb2400692c636738be215af69f
Kylie West <kmichellewest@gmail.com>
2023-03-21 07:41:45 -0700

    Uncomment code in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
 import Game from "./Game";
-
-import Home from "./Home";
 import Landing from "./Landing";
+import fetchFromSpotify, { request } from "../services/api";
+
+const AUTH_ENDPOINT =
+	"https://nuod0t2zoe.execute-api.us-east-2.amazonaws.com/FT-Classroom/spotify-auth-token";
+const TOKEN_KEY = "whos-who-access-token";
 
 const App = () => {
 	const [token, setToken] = useState(null);
@@ -16,47 +19,47 @@ const App = () => {
 		numArtists: 2
 	});
 
-	// const loadGenres = async t => {
-	//   setConfigLoading(true)
-	//   const response = await fetchFromSpotify({
-	//     token: t,
-	//     endpoint: 'recommendations/available-genre-seeds'
-	//   })
-	//   console.log(response)
-	//   setGenres(response.genres)
-	//   setConfigLoading(false)
-	// }
+	const loadGenres = async t => {
+		setConfigLoading(true);
+		const response = await fetchFromSpotify({
+			token: t,
+			endpoint: "recommendations/available-genre-seeds"
+		});
+		console.log(response);
+		setGenres(response.genres);
+		setConfigLoading(false);
+	};
 
-	// useEffect(() => {
-	// 	setAuthLoading(true);
+	useEffect(() => {
+		setAuthLoading(true);
 
-	// 	const storedTokenString = localStorage.getItem(TOKEN_KEY);
-	// 	if (storedTokenString) {
-	// 		const storedToken = JSON.parse(storedTokenString);
-	// 		if (storedToken.expiration > Date.now()) {
-	// 			console.log("Token found in localstorage");
-	// 			setAuthLoading(false);
-	// 			setToken(storedToken.value);
-	// 			loadGenres(storedToken.value);
-	// 			return;
-	// 		}
-	// 	}
-	// 	console.log("Sending request to AWS endpoint");
-	// 	request(AUTH_ENDPOINT).then(({ access_token, expires_in }) => {
-	// 		const newToken = {
-	// 			value: access_token,
-	// 			expiration: Date.now() + (expires_in - 20) * 1000
-	// 		};
-	// 		localStorage.setItem(TOKEN_KEY, JSON.stringify(newToken));
-	// 		setAuthLoading(false);
-	// 		setToken(newToken.value);
-	// 		loadGenres(newToken.value);
-	// 	});
-	// }, []);
+		const storedTokenString = localStorage.getItem(TOKEN_KEY);
+		if (storedTokenString) {
+			const storedToken = JSON.parse(storedTokenString);
+			if (storedToken.expiration > Date.now()) {
+				console.log("Token found in localstorage");
+				setAuthLoading(false);
+				setToken(storedToken.value);
+				loadGenres(storedToken.value);
+				return;
+			}
+		}
+		console.log("Sending request to AWS endpoint");
+		request(AUTH_ENDPOINT).then(({ access_token, expires_in }) => {
+			const newToken = {
+				value: access_token,
+				expiration: Date.now() + (expires_in - 20) * 1000
+			};
+			localStorage.setItem(TOKEN_KEY, JSON.stringify(newToken));
+			setAuthLoading(false);
+			setToken(newToken.value);
+			loadGenres(newToken.value);
+		});
+	}, []);
 
-	// if (authLoading || configLoading) {
-	//   return <div>Loading...</div>
-	// }
+	if (authLoading || configLoading) {
+		return <div>Loading...</div>;
+	}
 
 	return (
 		<div>
